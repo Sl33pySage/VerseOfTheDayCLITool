591bbe91823e30b33701ae3489ef36f9b09e2dc3
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-04-03 21:09:59 -0500

    Getting things ready..Gonna need a lot of socket research

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 int main() {
-  struct sockaddr_in sa;
-  inet_pton(AF_INET, "192.168.100.1", &(sa.sin_addr));
+  struct sockaddr_in address;
+  inet_pton(AF_INET, "192.168.100.1", &(address.sin_addr));
 }
