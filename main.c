7ae2821f9459b08f85e8e65d9a3ce389c0c3691c
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-04-05 15:13:43 -0500

    Adding time.h and the code to get current time

--- main.c
+++ main.c
@@ -3,9 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
 int main() {
+  // Getting the current date time
+  time_t now = time(NULL);
+  printf("The current time is: %s", ctime(&now));
+
   struct sockaddr_in address;
   inet_pton(AF_INET, "192.168.100.1", &(address.sin_addr));
 }
