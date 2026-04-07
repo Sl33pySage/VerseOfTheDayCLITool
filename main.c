6ee4127e18cf43c8465a4dda259cf228be4709c0
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-04-07 17:52:09 -0500

    Feat/networking (#9)
    * Researching on how to add the socket and networking code
    
    * on Chapter 5.1 on Beej's guide to network programming to try and implement this
    
    * feat: Chapt 5.2???
    
    * feat: On Chapt 5.2 frfr now and as a bonus I made the showip example that is pretty nifty >:3c
    
    * feat: ErrorChecks and various rabbitholes I been trying to go down to get this figured out
    
    * feat: Merge conflict x.x
    
    * feat: Deleting .bible_cache to mayhaps hopefully resolve the merge conflict
    
    * feat: At least this is giving me a pointer address..Had to delete a lot of lines and redo a lot to get it goin
    
    * removed .bible_cache
    
    * feat: FINALLY! got it to print out the ip address
    
    * fix: ok so merge conflicts on the .bible_cache and binary files
    
    * fix: Cleaning up main.c
    
    * feat: I think this is the way

--- main.c
+++ main.c
@@ -43,62 +43,15 @@ int main() {
   printf("Date and time successfully stored in .bible_cache\n");
 
   // Networking:
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> main
   // int getaddrinfo(
   //    const char *node,    // e.g. "https://bible-api.com/ or IP address"
   //    const char *service, // e.g. "http" or port number
   //    const struct addrinfo *hints, struct addrinfo **res);
 
-<<<<<<< HEAD
+  
   int status;
   struct addrinfo hints, *res, *p;
   char ipstr[INET6_ADDRSTRLEN];
-=======
-  // Client:
->>>>>>> main
-  int status;
-  struct addrinfo hints, *res, *p;
-  char ipstr[INET6_ADDRSTRLEN];
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  if ((status = getaddrinfo("bible-api.com", "80", &hints, &res)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    return 1;
-  };
-
-  for (p = res; p != NULL; p = p->ai_next) {
-    void *addr;
-    char *ipver;
-    // Extract IP based on address family
-    if (p->ai_family == AF_INET) { // IPv4
-      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-      addr = &(ipv4->sin_addr);
-      ipver = "IPv4";
-    }
-
-<<<<<<< HEAD
-    // Convert the IP to a string and print
-    inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-    printf(" %s: %s\n", ipver, ipstr);
-  }
-  freeaddrinfo(res);
-  return 0;
-
-=======
-  // Client:
->>>>>>> main
-  int status;
-  struct addrinfo hints, *res, *p;
->>>>>>> main
 
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
@@ -125,11 +78,7 @@ int main() {
   }
   freeaddrinfo(res);
   return 0;
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
->>>>>>> main
+  
   /*
  int getaddrinfo(const char *node,    // e.g., "www.example.com" or IP Address
                  const char *service, // e.g., "http" or port number "80"
@@ -147,17 +96,11 @@ int main() {
    char *ai_canonname;
    struct addrinfo *ai_next;
  };
-<<<<<<< HEAD
-=======
-*/
-=======
   // 1. Create a socket
   // int socket(int domain, int type, int protocol);
   // struct sockaddr_in address;
->>>>>>> main
->>>>>>> main
+
   // 192.42.93.30
   // inet_pton(AF_INET, "192.168.100.1", &(address.sin_addr));
 */
->>>>>>> main
 }
