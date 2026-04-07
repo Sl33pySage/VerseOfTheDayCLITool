e0c9c61624f4a7e758a5d018f244f959091913ed
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-04-07 01:50:19 -0500

    fix: Cleaning up main.c

--- main.c
+++ main.c
@@ -43,17 +43,11 @@ int main() {
   printf("Date and time successfully stored in .bible_cache\n");
 
   // Networking:
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
   // int getaddrinfo(
   //    const char *node,    // e.g. "https://bible-api.com/ or IP address"
   //    const char *service, // e.g. "http" or port number
   //    const struct addrinfo *hints, struct addrinfo **res);
 
-  // Client:
->>>>>>> main
   int status;
   struct addrinfo hints, *res, *p;
   char ipstr[INET6_ADDRSTRLEN];
@@ -77,64 +71,10 @@ int main() {
       ipver = "IPv4";
     }
 
-<<<<<<< HEAD
     // Convert the IP to a string and print
     inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
     printf(" %s: %s\n", ipver, ipstr);
   }
   freeaddrinfo(res);
   return 0;
-
-=======
-  // Client:
->>>>>>> main
-  int status;
-  struct addrinfo hints, *res, *p;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  if ((status = getaddrinfo("bible-api.com", "80", &hints, &res)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    return 1;
-  };
-
-  for (p = res; p != NULL; p = p->ai_next) {
-    printf("PF: %p\n", p->ai_addr);
-  }
-  freeaddrinfo(res);
-  return 0;
-
-<<<<<<< HEAD
->>>>>>> main
-  /*
- int getaddrinfo(const char *node,    // e.g., "www.example.com" or IP Address
-                 const char *service, // e.g., "http" or port number "80"
-                 const struct addrinfo *hints, // criteria for results
-                 struct addrinfo **res);       // pointer to the resulting list
- // struct addrinfo hints, *res;
- // struct sockaddr_in address;
- struct addrinfo {
-   int ai_flags;
-   int ai_family;
-   int ai_socktype;
-   int ai_protocol;
-   socklen_t ai_addrlen;
-   struct sockaddr *ai_addr;
-   char *ai_canonname;
-   struct addrinfo *ai_next;
- };
-<<<<<<< HEAD
-=======
-*/
-=======
-  // 1. Create a socket
-  // int socket(int domain, int type, int protocol);
-  // struct sockaddr_in address;
->>>>>>> main
->>>>>>> main
-  // 192.42.93.30
-  // inet_pton(AF_INET, "192.168.100.1", &(address.sin_addr));
-*/
 }
