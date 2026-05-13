a7bd00ee4feb0fae5254277495ba79eed6b0b7c8
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-05-13 12:19:35 -0500

    Refactor branch (#66)
    * Cleaning up and now trying to format the verse string
    
    * refactor: Getting the correct format to print and storing them in the proper variable names
    
    * refactor: Got the verse, book, and chapter number but trying to get the translation formatted
    
    * refactor: Cleaned up the formatting. Now to Refactor the code
    
    * refactor: Gonna clean up the state machine func

--- main.c
+++ main.c
@@ -44,9 +44,10 @@ void state_machine() {
 
   // 6. Close the file
   fclose(fptr);
-  printf("Date and time successfully stored in .bible_cache: 'Last Sucessfully "
-         "Ran: %s'\n",
-         buffer);
+  printf(
+      "Date and time successfully stored in .bible_cache:\n'Last Sucessfully "
+      "Ran: %s'\n",
+      buffer);
 }
 
 void lib_curl_func() {
@@ -86,6 +87,54 @@ static size_t cb(char *data, size_t size, size_t nmemb, void *clientp) {
 struct memory chunk = {0};
 CURLcode result;
 
+void formatfunc(cJSON *Bible_Data) {
+  /*
+   * Takes in the json object taken from the API as a argument
+   *
+   * Returns a string formatted with the Bible Data
+   */
+
+  // Parse the JSON Object
+  Bible_Data = cJSON_Parse(chunk.response);
+  cJSON *random_verse_object =
+      cJSON_GetObjectItemCaseSensitive(Bible_Data->child, "random_verse");
+
+  const char *translation_data = cJSON_Print(Bible_Data->child);
+  // printf("translation_data: %s\n", translation_data);
+
+  // Translation Data Object
+  cJSON *Version = cJSON_GetObjectItemCaseSensitive(Bible_Data->child, "name");
+  const char *version = cJSON_Print(Version);
+
+  // Random Verse Object Data
+  const char *random_verse_data = cJSON_Print(Bible_Data->child->next);
+  // printf("random_verse_data: %s\n", random_verse_data);
+
+  // Book Name Data
+  cJSON *book_Name =
+      cJSON_GetObjectItemCaseSensitive(Bible_Data->child->next, "book");
+  const char *book = cJSON_Print(book_Name);
+
+  // Chapter Number Data
+  cJSON *chapter_Number =
+      cJSON_GetObjectItemCaseSensitive(Bible_Data->child->next, "chapter");
+  const char *chapter_num = cJSON_Print(chapter_Number);
+
+  // Verse Number Data
+  cJSON *number =
+      cJSON_GetObjectItemCaseSensitive(Bible_Data->child->next, "verse");
+  const char *num = cJSON_Print(number);
+
+  // Verse Text String Data
+  cJSON *text =
+      cJSON_GetObjectItemCaseSensitive(Bible_Data->child->next, "text");
+  const char *verse = cJSON_Print(text);
+
+  // Final Output String
+  printf("%s (%s) %s:%s\n%s\n", book, version, chapter_num, num,
+         text->valuestring);
+}
+
 void api_call() {
   CURL *curl = curl_easy_init();
   if (curl) {
@@ -103,52 +152,7 @@ void api_call() {
     // Send a request
     result = curl_easy_perform(curl);
 
-    cJSON *jsond = cJSON_Parse(chunk.response);
-    cJSON *random_verse =
-        cJSON_GetObjectItemCaseSensitive(jsond->child, "random_verse");
-
-    const char *Bible_Data = cJSON_Print(jsond);
-    // printf("Bible_Data: %s\n", Bible_Data);
-
-    const char *translation_data = cJSON_Print(jsond->child);
-    // printf("translation_data: %s\n", translation_data);
-
-    // Translation Data Object
-    cJSON *Version = cJSON_GetObjectItemCaseSensitive(jsond->child, "name");
-    const char *version = cJSON_Print(Version);
-
-    // Random Verse Object Data
-    const char *random_verse_data = cJSON_Print(jsond->child->next);
-    // printf("random_verse_data: %s\n", random_verse_data);
-
-    // Book Name Data
-    cJSON *book_Name =
-        cJSON_GetObjectItemCaseSensitive(jsond->child->next, "book");
-    const char *book = cJSON_Print(book_Name);
-
-    // Chapter Number Data
-    cJSON *chapter_Number =
-        cJSON_GetObjectItemCaseSensitive(jsond->child->next, "chapter");
-    const char *chapter_num = cJSON_Print(chapter_Number);
-
-    // Verse Number Data
-    cJSON *number =
-        cJSON_GetObjectItemCaseSensitive(jsond->child->next, "verse");
-    const char *num = cJSON_Print(number);
-
-    // Verse Text String Data
-    cJSON *text = cJSON_GetObjectItemCaseSensitive(jsond->child->next, "text");
-    const char *verse = cJSON_Print(text);
-
-    // Final Output String
-    printf("%s (%s) %s:%s\n%s\n", book, version, chapter_num, num,
-           text->valuestring);
-
-    cJSON *string = cJSON_CreateString(verse);
-    const char *s = cJSON_Print(string);
-
-    // fprintf(s, "s: %c");
-
+    formatfunc(curl);
     // Remember to free the buffer
     free(chunk.response);
     curl_easy_cleanup(curl);
